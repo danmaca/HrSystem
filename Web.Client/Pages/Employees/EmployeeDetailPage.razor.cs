9b5603c2856b7f1ea3e1db629c3078aafeae1642
Daniel Macek <danmaca@centrum.cz>
2024-09-01 10:00:56 +0200

    employee component move

---
+++ Web.Client/Pages/Employees/EmployeeDetailPage.razor.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using Havit.Bonusario.Web.Client.DataStores;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace DanM.HrSystem.Web.Client.Pages.Employees;
+
+public partial class EntryCard
+{
+	[Parameter] public EmployeeGridDto Entry { get; set; }
+	[Parameter] public bool RecipientLocked { get; set; } = false;
+	[Parameter] public bool ShowAuthor { get; set; } = false;
+	[Parameter] public EventCallback OnEntryDeleted { get; set; }
+	[Parameter] public EventCallback<EntryDto> OnEntryCreated { get; set; }
+	[Parameter] public EventCallback<EntryDto> OnEntryUpdated { get; set; }
+	[Parameter] public EventCallback OnCloseButtonClicked { get; set; }
+	[Parameter] public string CssClass { get; set; }
+
+	[Inject] protected IEmployeeFacade EmployeeFacade { get; set; }
+	[Inject] protected IEmployeesDataStore EmployeesDataStore { get; set; }
+	[Inject] protected IEntryFacade EntryFacade { get; set; }
+
+	private EditContext editContext;
+
+	private bool RenderAuthor => ShowAuthor && Entry.CreatedById.HasValue;
+	/// <summary>
+	/// Indicates whether the entry is being edited or created.
+	/// If <c>true</c>, it is being edited. If <c>false</c> it is a new entry.
+	/// </summary>
+	private bool EdittingEntry => Entry.Id != default;
+	private bool RenderCloseButton => OnCloseButtonClicked.HasDelegate;
+
+	protected override void OnInitialized()
+	{
+		editContext = new(new EntryDto());
+	}
+
+	protected override void OnParametersSet()
+	{
+		editContext = new EditContext(Entry);
+	}
+
+	protected override async Task OnInitializedAsync()
+	{
+		await EmployeesDataStore.EnsureDataAsync();
+	}
+
+	private async Task HandleDeleteClick()
+	{
+		Contract.Assert(Entry.Submitted is null, "Nelze smazat odeslaný záznam.");
+		await EntryFacade.DeleteEntryAsync(Dto.FromValue(Entry.Id));
+		await OnEntryDeleted.InvokeAsync();
+	}
+
+	private async Task HandleCreateOrUpdateButtonClick()
+	{
+		if (Entry.Id == default && Entry.Submitted is null)
+		{
+			await CreateNewEntry();
+		}
+		else
+		{
+			await UpdateEntry();
+		}
+	}
+
+	private async Task CreateNewEntry()
+	{
+		Contract.Assert(Entry.Id == default, "Záznam již není nový.");
+		Contract.Assert(Entry.PeriodId != default, "PeriodId musí být nastaven.");
+
+		try
+		{
+			this.Entry.Id = (await EntryFacade.CreateEntryAsync(this.Entry)).Value;
+			await OnEntryCreated.InvokeAsync(this.Entry);
+		}
+		catch (OperationFailedException)
+		{
+			// NOOP
+		}
+	}
+
+	private async Task UpdateEntry()
+	{
+		if ((Entry.Id == default) || (Entry.Submitted is not null))
+		{
+			return;
+		}
+
+		if (editContext.Validate())
+		{
+			try
+			{
+				await EntryFacade.UpdateEntryAsync(this.Entry);
+				await OnEntryUpdated.InvokeAsync(this.Entry);
+			}
+			catch (OperationFailedException)
+			{
+				// NOOP
+			}
+		}
+	}
+
+	private async Task HandleCloseButtonClick()
+	{
+		await OnCloseButtonClicked.InvokeAsync();
+	}
+}
---
+++ Web.Client/Pages/Employees/EmployeeGridTable.razor.cs
@@ -0,0 +1,30 @@
+﻿using Havit;
+
+namespace DanM.HrSystem.Web.Client.Pages.Employees;
+
+public partial class EmployeeGridTable
+{
+	[Inject] protected IEmployeeFacade EmployeeFacade { get; set; }
+	[Inject] protected IHxMessageBoxService MessageBox { get; set; }
+
+	private List<EmployeeGridDto> gridData;
+	private HxGrid<EmployeeGridDto> conGrid;
+
+	 private async Task<GridDataProviderResult<EmployeeGridDto>> GetDataAsync(GridDataProviderRequest<EmployeeGridDto> request)
+	{
+		try
+		{
+			gridData = await EmployeeFacade.GetAllEmployeesAsync();
+			return request.ApplyTo(gridData);
+		}
+		catch (OperationFailedException)
+		{
+			return new() { Data = null, TotalCount = 0 };
+		}
+	}
+
+	private async Task GridItemSelected(EmployeeGridDto item)
+	{
+		await MessageBox.ConfirmAsync("Potvrzení", "Opravdu si přejete všechny koncepty potvrdit?");
+	}
+}
